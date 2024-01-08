fb1821dbb057b1500bd27760fe060bc93af77167
mdasik666 <asik301998@gmail.com>
2024-01-08 23:49:56 +0530

    second

---
+++ static/js/972.917dedb7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkss_react=self.webpackChunkss_react||[]).push([[972],{6972:(s,e,c)=>{c.r(e),c.d(e,{default:()=>a});var r=c(184);const a=()=>(0,r.jsx)("div",{children:"My profile"})}}]);
+//# sourceMappingURL=972.917dedb7.chunk.js.map
